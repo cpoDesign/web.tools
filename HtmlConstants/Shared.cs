e61754bbaeb7566f9082818b5864b16a1c9c5b2f
Pavel <cpoDesign@users.noreply.github.com>
2017-02-03 13:07:37 +0000

    Update Shared.cs
    Added application xml

--- HtmlConstants/Shared.cs
+++ HtmlConstants/Shared.cs
@@ -1,5 +1,6 @@
 namespace CpoDesign.Tools.HtmlConstants{
   public static class HtmlConstants{
     public static ApplicationJson = "application/json";
+    public static ApplicationXml = "application/xml";
   }
 }
